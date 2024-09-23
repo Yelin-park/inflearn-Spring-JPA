c1441c3818e6a24f73a66cbc861ae0d4171118c1
Yaliny <mae1217@naver.com>
2024-09-23 14:14:30 +0000

    feat(Entity Class): 구현중

---
+++ jpa_application_1/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ORDER 테이블에서 MEMBER와 DELIVERY의 FK를 가지고 있어서 연관관계의 주인이 된다.
+ * **/
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id") // FK
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
