1a2cf8f8494c1eef8687e8ea2407ed8dcbe10760
Yaliny <mae1217@naver.com>
2024-09-29 08:56:02 +0000

    feat(Item): 상품 엔티티 개발

--- jpa_application_1/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpa_application_1/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,17 +2,19 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Setter를 가지고 밖에서 계산하는 것이 아니라 엔티티 안에 비즈니스 로직을 만들어서 처리하는 것이 객체지향적이고 응집도가 높다.
+ * */
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
 public abstract class Item {
 
     @Id
@@ -28,4 +30,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //===비즈니스 로직===//
+    /**
+     * stock 증가
+     * **/
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
