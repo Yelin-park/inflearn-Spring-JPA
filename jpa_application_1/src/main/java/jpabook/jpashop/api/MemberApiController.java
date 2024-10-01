c963e6172fcd8f26fd1bdc4b8b5cd94cc285e926
Yaliny <mae1217@naver.com>
2024-10-01 13:21:09 +0000

    feat(Member): 회원 등록, 수정, 조회 API 구현 완료
    JPA 활용 2편 시작

---
+++ jpa_application_1/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,150 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    /**
+     * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
+     * - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한 모든 요청 요구사항을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * 결론
+     * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
+     */
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    /**
+     * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
+     *
+     * `CreateMemberRequest` 를 `Member` 엔티티 대신에 RequestBody와 매핑한다.
+     * 엔티티와 프레젠테이션 계층을 위한 로직을 분리할 수 있다.
+     * 엔티티와 API 스펙을 명확하게 분리할 수 있다.
+     * 엔티티가 변해도 API 스펙이 변하지 않는다.
+     * 참고: 실무에서는 엔티티를 API 스펙에 노출하면 안된다!
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setUsername(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @PatchMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request
+    ) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getUsername());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    /**
+     * 조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     *  좋지않은 버전임. 모든 엔티티가 노출되고, 엔티티에 @JsonIgnore 이건 정말 최악이다. api가 이거 하나가 아님. 화면에 종속적이지 말기
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 기본적으로 엔티티의 모든 값이 노출된다.
+     * - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등등)
+     * - 실무에서는 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * - 추가로 컬렉션을 직접 반환하면 항후 API 스펙을 변경하기 어렵다.(별도의 Result 클래스 생성으로 해결)
+     * 결론
+     * - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    /**
+     * 조회 V2: 응답 값으로 엔티티가 아닌 별도의 DTO를 반환한다.
+     *
+     * 엔티티를 DTO로 변환해서 반환한다.
+     * 엔티티가 변해도 API 스펙이 변경되지 않는다.
+     * 추가로 `Result` 클래스로 컬렉션을 감싸서 향후 필요한 필드를 추가할 수 있다.
+     */
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getUsername()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    /**
+     * Result 클래스로 감싸지 않고 그냥 내보냈을 때 차이점을 확인하기 위한 API
+     */
+    @GetMapping("/api/v3/members")
+    public List<MemberDto> membersV3() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getUsername()))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+}
