80f40b1667755bb9d0b213c5fa43b2e6710c97c5
Yaliny <mae1217@naver.com>
2024-10-05 09:00:46 +0000

    feat(간단한 주문 조회 API): V2 엔티티를 DTO로 변환하는 일반적인 방법(fetch join 사용 x)

--- jpa_application_1/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpa_application_1/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(컬렉션이 아닌 관계)
@@ -34,5 +39,36 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
         return all;
+    }
+
+    /**
+     * 간단한 주문 조회 V2 : 엔티티를 DTO로 변환
+     * N+1 문제 발생
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        /*return orderRepository.findAllByString(new OrderSearch()).stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());*/
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream().map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getUsername(); // Lazy 강제 초기화
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
     }
 }
